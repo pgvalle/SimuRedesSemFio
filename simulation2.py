63bc2794016733a28915a03525d5632ab95c7e26
pgvalle <pgvalle.nogueira@gmail.com>
2025-12-07 19:09:01 -0300

    backup in case of needing to redo work

---
+++ simulation2.py
@@ -0,0 +1,187 @@
+from ns import ns
+import csv
+import scipy.stats as sci
+import numpy as np
+
+SEED = 1
+ROUNDS = 10
+DURATION = 60
+
+TCPS = ['NewReno', 'Vegas', 'Veno', 'WestwoodPlus']
+BERS = [1e-5, 1e-4, 1e-3]
+MIN_DELAY = 5
+MAX_DELAY = 20
+
+cpp = ns.cppyy
+cpp.cppdef('''
+        using namespace ns3;
+
+        template<typename... Args>
+        Callback<void, Args...> MakeMyCallback(void(*function)(Args...))
+        {
+            return MakeCallback(function);
+        }
+           ''')
+
+def Main():
+    file = open('results1.csv', 'a+', newline='')
+    writer = csv.writer(file)
+
+    # the file is new. Write the column names
+    if file.tell() == 0:
+        writer.writerow(['tcp', 'ber', 'mean', 'off99', 'off95'])
+
+    ns.RngSeedManager.SetSeed(SEED)
+
+    for tcp in TCPS:
+        ns.Config.SetDefault('ns3::TcpL4Protocol::SocketType',
+                             ns.StringValue('ns3::Tcp' + tcp))
+
+        for ber in BERS:
+            print(f'params: tcp={tcp}, delay={delay}, ber={ber}')
+
+            throughputs = []
+            for i in range(1, ROUNDS + 1):
+                ns.RngSeedManager.SetRun(i)
+                throughput = Simulate(ber)
+                throughputs.append(throughput)
+                print(f'{throughput}')
+
+            mean = np.mean(throughputs)
+            off99 = ConfidenceOffset(throughputs, confidence=0.99)
+            off95 = ConfidenceOffset(throughputs, confidence=0.95)
+            writer.writerow([tcp, ber, delay, mean, off99, off95])
+            print(f' mean: {mean}, off99: {off99}, off95 {off95}')
+
+    file.close()
+
+
+def ConfidenceOffset(samples, confidence=0.95):
+    alpha = 1 - confidence
+    quantile = 1 - alpha / 2
+
+    n = len(samples)
+    t = sci.t.ppf(quantile, df=n-1)
+    offset = t * np.std(samples, ddof=1) / np.sqrt(n)
+
+    return offset
+
+
+def Simulate(ber):
+    # nodes
+    nodes = ns.NodeContainer()
+    nodes.Create(3)
+    sta = nodes.Get(0)
+    ap = nodes.Get(1)
+    server = nodes.Get(2)
+
+    # all nodes are static
+    mobility = ns.MobilityHelper()
+    mobility.SetMobilityModel('ns3::ConstantPositionMobilityModel')
+    mobility.SetPositionAllocator('ns3::GridPositionAllocator',
+                                  'DeltaX', ns.DoubleValue(10))
+    mobility.Install(nodes)
+
+    # variable delay
+    delayVar = ns.CreateObject[ns.UniformRandomVariable]()
+    delayVar.SetAttribute('Min', MIN_DELAY)
+    delayVar.SetAttribute('Max', MAX_DELAY)
+    # wifi with no signal loss and variable delay
+    wifiChannel = ns.YansWifiChannelHelper()
+    wifiChannel.SetPropagationDelay('ns3::RandomPropagationDelayModel',
+                                    'Variable', ns.PointerValue(delayVar))
+    wifiChannel.AddPropagationLoss('ns3::FixedRssLossModel',
+                                   'Rss', ns.DoubleValue(0.0))
+
+    # artificial ber
+    berVar = ns.CreateObject[ns.RateErrorModel]()
+    berVar.SetRate(ber)
+    berVar.SetUnit(ns.RateErrorModel.ERROR_UNIT_BIT)
+
+    wifiPhy = ns.YansWifiPhyHelper()
+    wifiPhy.SetChannel(wifiChannel.Create())
+    wifiPhy.SetErrorRateModel('ns3::YansErrorRateModel')
+    wifiPhy.Set('PostReceptionErrorModel', ns.PointerValue(berVar))
+
+    wifi = ns.WifiHelper()
+    wifi.SetStandard(ns.WIFI_STANDARD_80211ac)
+    wifiMac = ns.WifiMacHelper()
+
+    # station nic
+    wifiMac.SetType('ns3::StaWifiMac')
+    staDev = wifi.Install(wifiPhy, wifiMac, sta)
+    # ap nic
+    wifiMac.SetType('ns3::ApWifiMac')
+    apDev = wifi.Install(wifiPhy, wifiMac, ap)
+    # all wifi nics
+    wifiDevs = ns.NetDeviceContainer()
+    wifiDevs.Add(staDev)
+    wifiDevs.Add(apDev)
+
+    # ethernet nodes
+    csmaNodes = ns.NodeContainer()
+    csmaNodes.Add(ap)
+    csmaNodes.Add(server)
+    # ethernet
+    csma = ns.CsmaHelper()
+    csma.SetChannelAttribute('DataRate', ns.StringValue('100Mbps'))
+    csma.SetChannelAttribute('Delay', ns.TimeValue(ns.Time("1ms")))
+    # ethernet nics
+    csmaDevs = csma.Install(csmaNodes)
+
+    addr = ns.Ipv4AddressHelper()
+    stack = ns.InternetStackHelper()
+    stack.Install(nodes)
+    # put an ip on the wifi nics
+    addr.SetBase('10.1.1.0', '255.255.255.0')
+    wifiInterfaces = addr.Assign(wifiDevs)
+    # put an ip on the ethernet nics
+    addr.SetBase('10.1.2.0', '255.255.255.0')
+    csmaInterfaces = addr.Assign(csmaDevs)
+
+    ns.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
+
+    # server ip address with port
+    serverIp = csmaInterfaces.GetAddress(1)
+    dest = ns.InetSocketAddress(serverIp, port=5000).ConvertTo()
+    # bulk application on ap
+    bulk = ns.BulkSendHelper('ns3::TcpSocketFactory', dest)
+    bulk.SetAttribute('SendSize', ns.UintegerValue(1400))
+    bulkApp = bulk.Install(sta)
+    # sink application on server
+    sink = ns.PacketSinkHelper('ns3::TcpSocketFactory', dest)
+    sinkApp = sink.Install(server)
+
+    global timeFirstTx, timeLastRx
+    timeFirstTx = ns.Time(0)
+    timeLastRx = ns.Time(0)
+
+    def TxCallback(pkt: ns.Ptr[ns.Packet]) -> None:
+        global timeFirstTx
+        if timeFirstTx == ns.Time(0):
+            timeFirstTx = ns.Simulator.Now()
+
+    def RxCallback(pkt: ns.Ptr[ns.Packet], addr: ns.Address) -> None:
+        global timeLastRx
+        timeLastRx = ns.Simulator.Now()
+
+    bulkRef = bulkApp.Get(0)
+    bulkRef.TraceConnectWithoutContext('Tx', cpp.gbl.MakeMyCallback['Ptr<const Packet>'](TxCallback))
+    sinkRef = sinkApp.Get(0)
+    sinkRef.TraceConnectWithoutContext('Rx', cpp.gbl.MakeMyCallback['Ptr<const Packet>', 'const Address&'](RxCallback))
+
+    sinkApp.Start(ns.Seconds(0))
+    bulkApp.Start(ns.Seconds(1))
+
+    ns.Simulator.Stop(ns.Seconds(DURATION + 1))
+    ns.Simulator.Run()
+
+    rx = sinkRef.GetTotalRx()
+    delta = (timeLastRx - timeFirstTx).GetSeconds()
+    throughput = 8e-3 * (rx / delta) if delta > 0 else 0
+
+    ns.Simulator.Destroy()
+    return throughput
+
+
+Main()
